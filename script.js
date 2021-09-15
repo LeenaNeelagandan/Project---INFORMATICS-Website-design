2f9e03efc20ff474bd9f54da9579181c5fe7818e
LeenaNeelagandan <83346446+LeenaNeelagandan@users.noreply.github.com>
2021-09-15 16:57:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+var indexValue = 0;
+function slideShow() {
+    setTimeout(slideShow, 1000);
+    var x;
+    const img = document.querySelectorAll(".bgg");
+    for(x = 0; x < img.length; x++){
+        img[x].style.display = "none";
+    }
+    indexValue++;
+    if(indexValue > img.length) {
+        indexValue = 1
+    }
+    img[indexValue -1].style.display = "block";
+}
+slideShow();
+
+
+function videoUrl(hmmm){
+    document.getElementById("sliderVideo").src = hmmm;
+}
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
